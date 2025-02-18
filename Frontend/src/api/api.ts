d75073cb68e5ec836f9d3b12165c330076369179
June.io <82361101+sbjunaid@users.noreply.github.com>
2025-02-18 09:57:57 +0530

    Update api.ts

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/holidays";
+const API_BASE_URL = "https://holiday-calender-production.up.railway.app";
 
 export const fetchHolidays = async () => {
   try {
